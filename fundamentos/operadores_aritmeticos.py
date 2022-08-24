119ccafa635d7de1eaef9198da72fd1c5b4aefe9
Willian <willianlider01@gmail.com>
2022-08-24 20:53:42 -0300

    Update operadores_aritmeticos.py

--- fundamentos/operadores_aritmeticos.py
+++ fundamentos/operadores_aritmeticos.py
@@ -1,15 +1,15 @@
-# %%
-print(2 + 3)
-print(4 - 7)
-print(2 * 5.3)
-print(9.4 / 3)
-print(9.4 // 3)
-print(2 ** 8)
-print(10 % 3)
+# # %%
+# print(2 + 3)
+# print(4 - 7)
+# print(2 * 5.3)
+# print(9.4 / 3)
+# print(9.4 // 3)
+# print(2 ** 8)
+# print(10 % 3)
 
-a = 12
-b = a
-print(a + b)
+# a = 12
+# b = a
+# print(a + b)
 
 # %%
 # Minhas variáveis
@@ -18,4 +18,4 @@ despesas = 2456.2
 
 # Resposta do desafio
 percentual_comprometido = despesas / salario * 100
-percentual_comprometido
+print(percentual_comprometido)
